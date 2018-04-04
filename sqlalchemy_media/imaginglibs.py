d1e41dfb10a0ef809eec1621447bd1b6ac2dba76
Lele Gaifax <lele@metapensiero.it>
2018-04-04 18:10:06 +0200

    Implement crop() Wand-compatible method
    This is a first draft, the main logic is there, but surely cries for lots of tests.

--- sqlalchemy_media/imaginglibs.py
+++ sqlalchemy_media/imaginglibs.py
@@ -76,6 +76,85 @@ def use_pil():
         def save(self, *, file):
             self._i.save(file, self.format)
 
+        def crop(self, **cropargs):
+            # TODO: this surely require many more tests!
+            image = self._i
+            iwidth = image.width
+            iheight = image.height
+            gravity = cropargs.get('gravity')
+            cleft = cropargs.get('left')
+            ctop = cropargs.get('top')
+            cright = cropargs.get('right')
+            cbottom = cropargs.get('bottom')
+            cwidth = cropargs.get('width')
+            cheight = cropargs.get('height')
+            if gravity is None:
+                if cleft is None:
+                    cleft = 0
+                if ctop is None:
+                    ctop = 0
+                if cwidth is not None and cright is not None:
+                    raise ValueError('Cannot specify both "width" and "right"')
+                if cright is None:
+                    if cwidth is None:
+                        cright = iwidth
+                    else:
+                        cright = cleft + cwidth
+                if cheight is not None and cbottom is not None:
+                    raise ValueError('Cannot specify both "height" and "bottom"')
+                if cbottom is None:
+                    if cheight is None:
+                        cbottom = iheight
+                    else:
+                        cbottom = ctop + cheight
+                box = (cleft, ctop, cright, cbottom)
+            else:
+                if cwidth is None or cheight is None:
+                    raise ValueError('When "gravity" is specified "width" and "height" are mandatory')
+                if gravity == 'north_west':
+                    box = (0, 0, cwidth, cheight)
+                elif gravity == 'north':
+                    hcut = iwidth - cwidth
+                    if hcut < 0:
+                        hcut = 0
+                    half_hcut = hcut / 2.0
+                    box = (half_hcut, 0, cwidth + half_hcut, cheight)
+                elif gravity == 'north_east':
+                    box = (iwidth - cwidth, 0, iwidth, cheight)
+                elif gravity == 'west':
+                    vcut = iheight - cheight
+                    if vcut < 0:
+                        vcut = 0
+                    half_vcut = vcut / 2.0
+                    box = (0, half_vcut, cwidth, cheight + half_vcut)
+                elif gravity == 'center':
+                    hcut = iwidth - cwidth
+                    if hcut < 0:
+                        hcut = 0
+                    half_hcut = hcut / 2.0
+                    vcut = iheight - cheight
+                    if vcut < 0:
+                        vcut = 0
+                    half_vcut = vcut / 2.0
+                    box = (half_hcut, half_vcut, cwidth + half_hcut, cheight + half_vcut)
+                elif gravity == 'east':
+                    vcut = iheight - cheight
+                    if vcut < 0:
+                        vcut = 0
+                    half_vcut = vcut / 2.0
+                    box = (iwidth - cwidth, half_vcut, iwidth, cheight + half_vcut)
+                elif gravity == 'south_west':
+                    box = (0, iheight - cheight, cwidth, iheight)
+                elif gravity == 'south':
+                    hcut = iwidth - cwidth
+                    if hcut < 0:
+                        hcut = 0
+                    half_hcut = hcut / 2.0
+                    box = (half_hcut, iheight - cheight, cwidth + half_hcut, iheight)
+                elif gravity == 'south_east':
+                    box = (iwidth - cwidth, iheight - cheight, iwidth, iheight)
+            self._i = self._i.crop(box)
+
     ImageFactory = WandedPILImage
 
     return WandedPILImage
